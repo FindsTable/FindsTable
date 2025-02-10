9b059ae1dda59f9014b043a979a351a1afb6b78c
Eric <73071377+Posoroko@users.noreply.github.com>
2025-02-10 13:13:04 -0500

    thoughts pages and new though rendered seperatly

--- components/Pages/Home/Thoughts/Main.vue
+++ components/Pages/Home/Thoughts/Main.vue
@@ -1,38 +1,54 @@
 <script setup>
+const requestOffset = ref(0)
+const requestLimit = ref(5)
 
-const { data :  thoughts, refresh } = await useAsyncData(
-    'thoughts',
-    async () => {
-        const res = await useNuxtApp().$items.getByQuery({
-            collection: 'Thoughts',
-            query: {
-                fields: '*,user_created.avatars.*,user_created.id,user_created.displayName,user_created.username,date_created',
-                deep: {
-                    user_created: {
-                        avatars: {
-                            _sort: "-currentAt",
-                            _limit: 1
-                        }
+const thoughts = ref([])
+const newThoughts = ref([])
+
+async function getThoughts() {
+
+    const res = await useNuxtApp().$items.getByQuery({
+        collection: 'Thoughts',
+        query: {
+            fields: '*,user_created.avatars.*,user_created.id,user_created.displayName,user_created.username,date_created',
+            deep: {
+                user_created: {
+                    avatars: {
+                        _sort: "-currentAt",
+                        _limit: 1
                     }
-                },
-                sort: "-date_created"
-            }
-        })
-        if(res?.data) {
-            return res.data
+                }
+            },
+            sort: "-date_created",
+            offset: requestOffset.value,
+            limit: requestLimit.value
         }
+    })
+
+    if (res?.data) {
+        thoughts.value = [
+            ...thoughts.value,
+            ...res.data
+        ]
     }
-)
 
-function newThoughtPosted(newThought) {
-    // two possible options: refrech the request with no rerender, or 
-    // add the new item and trigger a full rerender...
-
-    //unshift in and rerender
-    // thoughts.value = [newThought, ...thoughts.value]
-    //refresh and update the dom
-    refresh()
 }
+function newThoughtPosted(newThought) {
+
+    newThoughts.value = [
+        ...newThoughts.value,
+        newThought
+    ]
+}
+
+function getNextPage() {
+    requestOffset.value+= requestLimit.value
+    getThoughts()
+}
+
+onMounted(() => {
+    getThoughts()
+})
 
 </script>
 
@@ -41,12 +57,29 @@ function newThoughtPosted(newThought) {
         @newThoughtPosted="newThoughtPosted"
     />
 
+    <div v-if="newThoughts">
+        <div
+            v-for="thought in newThoughts" :key="thought.id" 
+        >
+            <PagesHomeThoughtsThought :thought="thought" @thoughtDeleted="thoughtDeleted" />
+        </div>
+    </div>
+
     <div v-if="thoughts">
         <div
             v-for="thought in thoughts" :key="thought.id" 
         >
 
             <PagesHomeThoughtsThought :thought="thought" @thoughtDeleted="thoughtDeleted" />
+        </div>
+
+        <div class="centered">
+            <button 
+                class="comp-button -filled marTop20 font-text -main"
+                @click="getNextPage"
+            >
+                next page
+            </button>
         </div>
     </div>
 </template>
